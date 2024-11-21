13bb07353576b79d09e49c245178272cbcf5337b
michalkossakowski <141955539+michalkossakowski@users.noreply.github.com>
2024-11-21 21:45:10 +0100

    faq title validatior

--- src/app/faq/add-edit-faq/faqTitleExistsValidator.ts
+++ src/app/faq/add-edit-faq/faqTitleExistsValidator.ts
@@ -2,9 +2,7 @@ import { AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms';
 
 export function FaqTitleExistsValidator(existingTitles: string[]): ValidatorFn {
   return (control: AbstractControl): ValidationErrors | null => {
-    
-    const isTitleExists = existingTitles.some(title => title === control.value);
-
+    const isTitleExists = existingTitles.some(title => title.toLowerCase() === control.value.toLowerCase());
     return isTitleExists ? { 'titleExists': true } : null;
   };
 }
