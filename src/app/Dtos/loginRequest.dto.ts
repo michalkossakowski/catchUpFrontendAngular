6a6905fe6aedb3124c1b9e496d3263fb68d8ffb6
Kex <krzysieksobol9@gmail.com>
2024-11-30 23:53:16 +0100

    almost finished login and user, api calls doesnt work

---
+++ src/app/Dtos/loginRequest.dto.ts
@@ -0,0 +1,12 @@
+export class loginRequest {
+    email: string;
+    password: string;
+
+    constructor(
+        email:string,
+        password:string
+    ){
+        this.email = email;
+        this.password = password;
+    }
+}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,20 +7,44 @@ import { FaqDetailsComponent } from './faq/faq-details/faq-details.component';
 import { SchoolingsComponent } from './schoolings/schoolings.component';
 import { FeedbackComponent } from './feedback/feedback.component';
 import { LoginComponent } from './login/login.component';
+import { AuthGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
-    { path: '', component: LoginComponent },
-    { path: 'home', component:  HomeComponent },
-    { path: 'faq', component: FaqComponent },
-    { path: 'faq/details/:id', component: FaqDetailsComponent },
-    { path: 'admin-panel', component: AdminPanelComponent },
-    { path: 'schoolings', component: SchoolingsComponent },
-    { path: 'feedback', component: FeedbackComponent }
+  { path: '', component: LoginComponent }, // Login page
+  {
+    path: 'home',
+    component: HomeComponent,
+    canActivate: [AuthGuard] // Protect home route
+  },
+  {
+    path: 'faq',
+    component: FaqComponent,
+    canActivate: [AuthGuard] // Protect FAQ route
+  },
+  {
+    path: 'faq/details/:id',
+    component: FaqDetailsComponent,
+    canActivate: [AuthGuard] // Protect FAQ details route
+  },
+  {
+    path: 'admin-panel',
+    component: AdminPanelComponent,
+    canActivate: [AuthGuard] // Protect admin panel route
+  },
+  {
+    path: 'schoolings',
+    component: SchoolingsComponent,
+    canActivate: [AuthGuard] // Protect schoolings route
+  },
+  {
+    path: 'feedback',
+    component: FeedbackComponent,
+    canActivate: [AuthGuard] // Protect feedback route
+  }
 ];
 
 @NgModule({
-    imports: [RouterModule.forRoot(routes)],
-    exports: [RouterModule]
-  })
-
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
 export class AppRoutingModule { }
